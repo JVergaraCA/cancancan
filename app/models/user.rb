5aac39de5b9f8fb9550eef480e95b7337f464745
Juan Carlos Vergara <juanvergara9@hotmail.com>
2017-03-28 23:17:12 -0300

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+	has_many :posts, dependent: :destroy
+	has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
+   enum role: [:basic, :admin, :guest]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get'comments/index'
+
+  post 'comments/create'
+
+  put 'comments/update'
+
+  delete 'comments/destroy'
+
   devise_for :users
-  resources :posts
+  resources :posts do
+  	resources :comments, except: [:new, :show]
 
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
